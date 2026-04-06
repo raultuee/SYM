156ceb041f6a9306de096443c797d606e2458619
Raul Vinícius <165701907+raultuee@users.noreply.github.com>
2026-04-06 15:11:21 -0300

    fix: url button iniciar

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -165,7 +165,7 @@ export default function WelcomePage() {
           >
             NÚMEROS
           </a>
-          <Link to="/login">
+          <Link to="/register">
             <button
               style={{
                 background: "transparent",
